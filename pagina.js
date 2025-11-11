12f276836c9f790e24a2f402859240e3062a1294
JJzinho <joanjunior@gmail.com>
2025-11-10 22:37:12 -0300

    forns

--- pagina.js
+++ pagina.js
@@ -1,1 +1,15 @@
-document.getElementById("number") 
+document.addEventListener('DOMContentLoaded', () => {
+    const masks = {
+        nome: /[^a-zA-Z\s]/g,
+        quantidade: /[^0-9]/g,
+        pagar: /[^0-9]/g
+    };
+    Object.keys(masks).forEach(id => {
+        document.getElementById(id).addEventListener('input', e => {
+            e.target.value = e.target.value.replace(masks[id], '');
+        });
+    });
+    document.getElementById('enviarBtn').addEventListener('click', () => {
+        alert('pedido enviado com sucesso');
+    });
+});
